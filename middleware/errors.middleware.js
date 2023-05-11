cb440ff884cbdf8dd084ee99c883d8596aff6dc1
Kam Singh <kamisera+github@gmail.com>
2023-05-11 13:33:39 +0200

    add syntax error middleware

--- middleware/errors.middleware.js
+++ middleware/errors.middleware.js
@@ -15,6 +15,16 @@ exports.customErrors = (err, req, res, next) => {
   }
 };
 
+// handle invalid syntax in user submitted JSON data
+exports.invalidSubmittedData = (err, req, res, next) => {
+  if (err instanceof SyntaxError) {
+    return res
+      .status(400)
+      .send({ status: 400, message: "Invalid request body!" });
+  }
+  next(err);
+};
+
 // send internal server error status if the error cannot be categorised elsewhere
 exports.fallbackError = (err, req, res, next) => {
   res.status(500).send({
