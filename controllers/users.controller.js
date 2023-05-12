6cb7ac342883c67f56725965aa9ddf32f997ae41
Kam Singh <kamisera+github@gmail.com>
2023-05-12 16:42:45 +0200

    add missing catch blocks

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,9 @@
 const { fetchUsers } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
-  fetchUsers().then((users) => {
-    res.status(200).send(users);
-  });
+  fetchUsers()
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch(next);
 };
