559e8561c8121f902ca8f03a013828535c87befc
Kam Singh <kamisera+github@gmail.com>
2023-05-12 11:07:38 +0200

    add production settings

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
 const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, () => {
-  console.log("Listening on port 9090...");
+app.listen(PORT, (err) => {
+  if (err) console.log(error);
+  console.log(`Listening on port ${PORT}...`);
 });
